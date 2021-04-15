ab0f490af22bb80abc198dda7f9f310987b845b4
HideakiYamamoto <poo3gumi@gmail.com>
2021-04-15 14:59:12 +0900

    #1 ChatListControllerの作成等

---
+++ ChitChatClub/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChitChatClub
+//
+//  Created by 山本英明 on 2021/04/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
