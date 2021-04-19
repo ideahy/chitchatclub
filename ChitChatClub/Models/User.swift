55d710b607f59362a1397029d47f83c3ed8e1b00
HideakiYamamoto <poo3gumi@gmail.com>
2021-04-19 22:50:45 +0900

    #21 Userモデルの作成

---
+++ ChitChatClub/Models/User.swift
@@ -0,0 +1,27 @@
+//
+//  User.swift
+//  ChitChatClub
+//
+//  Created by 山本英明 on 2021/04/19.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestore
+
+class User {
+    
+    let email: String
+    let username: String
+    let createdAt: Timestamp
+    let profileImageUrl: String
+    
+    //上記の引数をもとにUser配列を作成
+    init(dic: [String: Any]) {
+        //??はnilだった場合の条件
+        self.email = dic["email"] as? String ?? ""
+        self.username = dic["username"] as? String ?? ""
+        self.createdAt = dic["createdAt"] as? Timestamp ?? Timestamp()
+        self.profileImageUrl = dic["profileImageUrl"] as? String ?? ""
+    }
+}
